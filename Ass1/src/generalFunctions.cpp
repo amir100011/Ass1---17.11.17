93de9af4ed88f50b2969a988f6c7e0bcafb227b1
amir100011 <33557685+amir100011@users.noreply.github.com>
2017-11-17 13:27:57 +0200

    Update generalFunctions.cpp

--- Ass1/src/generalFunctions.cpp
+++ Ass1/src/generalFunctions.cpp
@@ -23,57 +23,64 @@ vector<std::string>* pathSplit(string path){//general function returns pointer t
 
 }
 
-void jumpToNewWorkingDirectory(FileSystem& fs, string path) {
+Directory* jumpToNewWorkingDirectory(FileSystem& fs, string path) {
 
     string lastPathWord = path.substr(path.find_last_of("/") + 1, path.size());
 
     if (lastPathWord.compare((fs.getWorkingDirectory().getName())) == 0) {
         cout << "Can’t rename the working directory" << std::endl;
+        return nullptr;
     } else {
+        Directory* currentDir = &fs.getWorkingDirectory();
         if (path[0] == '/') {//this means from root
             vector<string> *brokenPath = pathSplit(path);
             Directory *rootFolder = &fs.getRootDirectory();
             Directory *isValid = rootFolder->pathValidation(brokenPath, 0);
             if (isValid != nullptr) {
-                fs.setWorkingDirectory(isValid);//TODO change name of last expression in path is exists
+                currentDir = isValid;//TODO change name of last expression in path is exists
+                return currentDir;
+            }else {
+                cout << "path is not valid" << endl;
+                return nullptr;
             }
         } else if (path[0] == '.' && path[1] == '.' && path[2] == '/') {//go up to parent directory
-            int i = 0;
-            int j = 1;
-            int k = 2;
-            while (path[i] == '.' && path[j] == '.' && path[k] == '/') {
-                fs.setWorkingDirectory(fs.getWorkingDirectory().getParent());
-                i += 3;
-                j += 3;
-                k += 3;
-                path = path.substr(k + 1, path.size());//cutting the expression "../"
+            while (path[0] == '.' && path[1] == '.' && path[2] == '/') {
+                currentDir = ((*currentDir).getParent());
+                path = path.substr(3, path.size());//cutting the expression "../"
             }
             vector<string> *brokenPath = pathSplit(path);
-            Directory *currentDir = &fs.getWorkingDirectory();
             Directory *isValid = currentDir->pathValidation(brokenPath, 0);
             if (isValid != nullptr) {
-                fs.setWorkingDirectory(isValid);
+                currentDir = isValid;
+                return currentDir;
+            }else{
+                cout << "path is not valid" <<endl;
+                return nullptr;
             }
         } else if (path[0] == '.' && path[1] == '/') {
             while (path[0] == '.' && path[1] == '/') {
                 path = path.substr(2, path.size());//cutting the expression "./"
             }
             vector<string> *brokenPath = pathSplit(path);
-            string x = (*brokenPath)[0];
-            string y = (*brokenPath)[1];
-            Directory *currentDir = &fs.getWorkingDirectory();
             Directory *isValid = currentDir->pathValidation(brokenPath, 0);
             if (isValid != nullptr) {
-                fs.setWorkingDirectory(isValid);
-            } else {//current directory
-                vector<string> *brokenPath = pathSplit(path);
-                Directory *currentDir = &fs.getWorkingDirectory();
-                Directory *isValid = currentDir->pathValidation(brokenPath, 0);
-                if (isValid != nullptr) {
-                    fs.setWorkingDirectory(isValid);
-                }
+                currentDir = isValid;
+                return currentDir;
+            } else{
+                cout << "path is not valid" <<endl;
+                return nullptr;
             }
-
+        }
+        else {//current directory
+            vector<string> *brokenPath = pathSplit(path);
+            Directory *isValid = currentDir->pathValidation(brokenPath, 0);
+            if (isValid != nullptr) {
+                currentDir = isValid;
+                return currentDir;
+            } else{
+                cout << "path is not valid" <<endl;
+                return nullptr;
+            }
         }
     }
 }

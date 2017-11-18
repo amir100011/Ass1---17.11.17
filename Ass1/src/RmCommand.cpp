ff33d6eb74779828e844aefd07fcd58a53e1c5b8
amir100011 <33557685+amir100011@users.noreply.github.com>
2017-11-18 20:10:13 +0200

    Update RmCommand.cpp

--- Ass1/src/RmCommand.cpp
+++ Ass1/src/RmCommand.cpp
@@ -15,21 +15,30 @@ void RmCommand::execute(FileSystem & fs){
 
     Directory* lastDir = &fs.getWorkingDirectory();
 
-    if (path[0] == '/')
+    if (path[0] == '/' && path.size() == 1)
         cout << "can't remove Root directory" << std::endl;
     else
         lastDir = jumpToNewWorkingDirectory(fs,path);
 
     if( lastDir != nullptr && (*lastDir).getChildren().size() > 0)
     {
+        bool okToDelete = false;
         string oldName = path.substr(path.find_last_of("/") + 1, path.size() );//last argument of the path + name to change to
         bool found = false;
         BaseFile* X = nullptr;
         for(int i = 0; i < (*lastDir).getChildren().size(); i++){
             X = (*lastDir).getChildren()[i];
             if(X->getName().compare(oldName) == 0) {
-                delete(X);
-                found = true;
+                if(X->isDirectory(X))//check if it is ok to delete X
+                     okToDelete = lastDir->checkSubTree(&fs.getWorkingDirectory(), dynamic_cast<Directory*>(X));
+                if (okToDelete) {
+                    (lastDir)->removeFile(X);
+                    found = true;
+                }
+                else {
+                    cout << "working directory is in the subtree of the path can't delete" << std::endl;
+                    found = true;
+                }
             }
         }
         if(!found)
